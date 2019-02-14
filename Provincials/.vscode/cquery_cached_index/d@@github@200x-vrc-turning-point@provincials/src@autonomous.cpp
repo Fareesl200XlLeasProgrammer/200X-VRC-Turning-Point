d85c5f130c77da6b88ae8a2ff42ed8d0033b8e3c
Farees|200X|LeadProgrammer <44035032+Fareesl200XlLeasProgrammer@users.noreply.github.com>
2019-02-13 21:45:49 -0500

    Added threshhold for brake system added gui fixed motor definitions

--- Provincials/.vscode/cquery_cached_index/d@@github@200x-vrc-turning-point@provincials/src@autonomous.cpp
+++ Provincials/.vscode/cquery_cached_index/d@@github@200x-vrc-turning-point@provincials/src@autonomous.cpp
@@ -11,4 +11,26 @@
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+  //Motor Definitions
+  pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+  pros::Motor Right(1, false);
+
+  pros::Motor Right2(2, false);
+
+  pros::Motor Left(3);
+
+  pros::Motor Left2(4);
+
+  pros::Motor Flipper(20);
+
+  pros::Motor Intake(7, false);
+
+  pros::Motor Lift(8);
+
+  pros::Motor Flywheel(10);
+
+  pros::Motor Index(17);
+  
+}
--- Provincials/src/opcontrol.cpp
+++ Provincials/src/opcontrol.cpp
@@ -1,8 +1,29 @@
 #include "main.h"
-
+#include "Gui.cpp"
 
 void opcontrol() {
-	#include "motordef.hpp"
+	//Motor Definitions
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+	pros::Motor Right(1, false);
+
+	pros::Motor Right2(2, false);
+
+	pros::Motor Left(3);
+
+	pros::Motor Left2(4);
+
+	pros::Motor Flipper(20);
+
+	pros::Motor Intake(7, false);
+
+	pros::Motor Lift(8);
+
+	pros::Motor Flywheel(10);
+
+	pros::Motor Index(17);
+
+	Gui();//Run the gui
 
 	//Setting Brake Modes
 	Lift.set_brake_mode(MOTOR_BRAKE_HOLD);
@@ -19,7 +40,7 @@ void opcontrol() {
     Left.move(master.get_analog(ANALOG_RIGHT_X)*0.5 + master.get_analog(ANALOG_LEFT_Y));
     Left2.move(master.get_analog(ANALOG_RIGHT_X)*0.5 + master.get_analog(ANALOG_LEFT_Y));
 
-		if(master.get_analog(ANALOG_RIGHT_X) ==0){
+		if(master.get_analog(ANALOG_LEFT_Y) < 5 && master.get_analog(ANALOG_LEFT_Y) >-5){
 			Right.set_brake_mode(MOTOR_BRAKE_HOLD);
 			Right2.set_brake_mode(MOTOR_BRAKE_HOLD);
 			Left.set_brake_mode(MOTOR_BRAKE_HOLD);
@@ -52,10 +73,6 @@ void opcontrol() {
       Index.move_velocity(200);
 		}
 
-    else if(master.get_digital(DIGITAL_L1) && master.get_digital(DIGITAL_L2)){
-      Intake.move_velocity(200);
-      Index.move_velocity(200);
-    }
     else{
       Intake.move_velocity(0);
       Index.move_velocity(0);
@@ -68,8 +85,6 @@ void opcontrol() {
 		if(master.get_digital(DIGITAL_B)){
 			Flywheel.move_velocity(120);
 		}
-
-
 
 		pros::delay(20);
 	}
