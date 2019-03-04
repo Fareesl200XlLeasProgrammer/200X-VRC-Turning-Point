aca9bbe43bed3db1d652f273538ae4200844763a
Farees|200X|LeadProgrammer <44035032+Fareesl200XlLeasProgrammer@users.noreply.github.com>
2019-03-03 21:03:29 -0500

    Fixed gui code made auto functions file

--- Skills/src/autonomous.cpp
+++ Skills/src/autonomous.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "AutoFunctions.cpp"
 #include "../include/display/lv_conf.h"
 #include "../include/display/lvgl.h"
 
@@ -10,27 +11,27 @@ static lv_res_t btn_click_action(lv_obj_t * btn1)
 
     if(id == 1){
       AutoCount = 1;
-      printf("Mode: %d Red Back\n", AutoCount);
+      printf("Mode %d: Red Back\n", AutoCount);
     }
 
     if(id == 2){
       AutoCount = 2;
-      printf("Mode: %d Red Front\n", AutoCount);
+      printf("Mode %d: Red Front\n", AutoCount);
     }
 
     if(id == 3){
       AutoCount = 3;
-      printf("Mode: %d Blue Back\n", AutoCount);
+      printf("Mode %d: Blue Back\n", AutoCount);
     }
 
     if(id == 4){
       AutoCount = 4;
-      printf("Mode: %d Blue Front\n", AutoCount);
+      printf("Mode %d: Blue Front\n", AutoCount);
     }
 
     if(id == 5){
       AutoCount = 5;
-      printf("Mode: %d Skills\n", AutoCount);
+      printf("Mode %d: Skills\n", AutoCount);
     }
 
     return LV_RES_OK; /*Return OK if the button is not deleted*/
@@ -110,293 +111,6 @@ void Gui(){
 	/*Add a label to the button*/
 	label = lv_label_create(btn5, NULL);
 	lv_label_set_text(label, "SKILLZ");
-}
-
-void Skills(){//Skills Auto
-	//Motor Definitions
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-	pros::Motor Right(1, false);
-
-	pros::Motor Right2(2, false);
-
-	pros::Motor Left(3);
-
-	pros::Motor Left2(4);
-
-	pros::Motor Flipper(20);
-
-	pros::Motor Intake(7, false);
-
-	pros::Motor Lift(8);
-
-	pros::Motor Flywheel(10);
-
-	pros::Motor Index(17);
-
-  Right.tare_position();
-  Right2.tare_position();
-  Left.tare_position();
-  Left2.tare_position();
-
-  Right.set_brake_mode(MOTOR_BRAKE_HOLD);
-  Right2.set_brake_mode(MOTOR_BRAKE_HOLD);
-  Left.set_brake_mode(MOTOR_BRAKE_HOLD);
-  Left2.set_brake_mode(MOTOR_BRAKE_HOLD);
-
-	Flywheel.move_velocity(135);
-	Intake.move_velocity(-200);
-
-  while(Left.get_position() < 3300){
-    Right.move_velocity(-150);
-    Right2.move_velocity(-150);
-    Left2.move_velocity(150);
-    Left.move_velocity(150);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-  pros::delay(1000);
-
-  while(Left.get_position() > 70){
-    Right.move_velocity(100);
-    Right2.move_velocity(100);
-    Left2.move_velocity(-100);
-    Left.move_velocity(-100);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-  pros::delay(500);
-
-
-  while(Left.get_position() > -610){
-    Right.move_velocity(-20);
-    Right2.move_velocity(-20);
-    Left2.move_velocity(-20);
-    Left.move_velocity(-20);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() < 3300){
-    Right.move_velocity(-100);
-    Right2.move_velocity(-100);
-    Left2.move_velocity(100);
-    Left.move_velocity(100);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	pros::delay(1000);
-
-	Index.move_relative(500, 200);
-
-	pros::delay(1000);
-
-	while(Left.get_position() < 4750){
-    Right.move_velocity(-100);
-    Right2.move_velocity(-100);
-    Left2.move_velocity(100);
-    Left.move_velocity(100);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	pros::delay(200);
-
-	Index.move_relative(7000, 200);
-	Flywheel.move_velocity(125);
-
-	pros::delay(500);
-
-	while(Left.get_position() > 2700){
-    Right.move_velocity(150);
-    Right2.move_velocity(150);
-    Left2.move_velocity(-150);
-    Left.move_velocity(-150);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() < 3150){
-    Right.move_velocity(30);
-    Right2.move_velocity(30);
-    Left2.move_velocity(30);
-    Left.move_velocity(30);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() < 6500){
-    Right.move_velocity(-150);
-    Right2.move_velocity(-150);
-    Left2.move_velocity(150);
-    Left.move_velocity(150);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	pros::delay(300);
-
-	while(Left.get_position() > 6000){
-    Right.move_velocity(100);
-    Right2.move_velocity(100);
-    Left2.move_velocity(-100);
-    Left.move_velocity(-100);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	pros::delay(300);
-
-	while(Left.get_position() > 5400){
-    Right.move_velocity(-20);
-    Right2.move_velocity(-20);
-    Left2.move_velocity(-20);
-    Left.move_velocity(-20);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	pros::delay(300);
-
-	Index.move_relative(4500, 200);
-
-	pros::delay(1000);
-
-	//////////////////////////////PARK///////////////////////////
-	while(Left.get_position() > 4750){
-    Right.move_velocity(-20);
-    Right2.move_velocity(-20);
-    Left2.move_velocity(-20);
-    Left.move_velocity(-20);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() < 8000){
-    Right.move_velocity(-200);
-    Right2.move_velocity(-200);
-    Left2.move_velocity(200);
-    Left.move_velocity(200);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() > 7300){
-    Right.move_velocity(200);
-    Right2.move_velocity(200);
-    Left2.move_velocity(-200);
-    Left.move_velocity(-200);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() > 6700){
-    Right.move_velocity(-20);
-    Right2.move_velocity(-20);
-    Left2.move_velocity(-20);
-    Left.move_velocity(-20);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-
-	while(Left.get_position() < 7500){
-    Right.move_velocity(-100);
-    Right2.move_velocity(-100);
-    Left2.move_velocity(100);
-    Left.move_velocity(100);
-  }
-  Right.move_velocity(0);
-  Right2.move_velocity(0);
-  Left2.move_velocity(0);
-  Left.move_velocity(0);
-}
-
-void Red_Front(){
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-	pros::Motor LeftF(1);
-
-	pros::Motor LeftB(2);
-
-	pros::Motor RightF(3, true);
-
-	pros::Motor RightB(4, true);
-
-	LeftF.move_relative(-500, 500);
-	LeftB.move_relative(-500, 500);
-	RightF.move_relative(-500, 500);
-	RightB.move_relative(-500, 500);
-}
-
-void Red_Back(){
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-	pros::Motor LeftF(1);
-
-	pros::Motor LeftB(2);
-
-	pros::Motor RightF(3, true);
-
-	pros::Motor RightB(4, true);
-
-	LeftF.move_relative(-500, 500);
-	LeftB.move_relative(500, 500);
-	RightF.move_relative(500, 500);
-	RightB.move_relative(-500, 500);
-
-}
-
-void Blue_Front(){
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-
-	pros::Motor LeftF(1);
-
-	pros::Motor LeftB(2);
-
-	pros::Motor RightF(3, true);
-
-	pros::Motor RightB(4, true);
-
-	LeftF.move_relative(500, 500);
-	LeftB.move_relative(500, 500);
-	RightF.move_relative(500, 500);
-	RightB.move_relative(500, 500);
-
-}
-
-void Blue_Back(){
-
 }
 
 /**
