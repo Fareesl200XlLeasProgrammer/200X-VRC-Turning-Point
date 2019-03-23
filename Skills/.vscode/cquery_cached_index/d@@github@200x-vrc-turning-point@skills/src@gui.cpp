8d14487a1c80bea2a96dc958d76debf7679c4ab5
Farees|200X|LeadProgrammer <44035032+Fareesl200XlLeasProgrammer@users.noreply.github.com>
2019-03-23 00:04:34 -0400

    cleaned up code

---
+++ Skills/.vscode/cquery_cached_index/d@@github@200x-vrc-turning-point@skills/src@gui.cpp
@@ -0,0 +1,90 @@
+#include "main.h"
+#include "D:/GitHub/200X-VRC-Turning-Point/Skills/include/display/lv_conf.h"
+#include "D:/GitHub/200X-VRC-Turning-Point/Skills/include/display/lvgl.h"
+
+static lv_res_t btn_click_action(lv_obj_t * btn1)
+{
+    uint8_t id = lv_obj_get_free_num(btn1);
+
+    printf("Button %d is released\n", id);
+
+    /* The button is released.
+     * Make something here */
+
+    return LV_RES_OK; /*Return OK if the button is not deleted*/
+}
+
+void Gui(){
+	lv_theme_t *th = lv_theme_alien_init(100, NULL);
+
+	/*Create a Tab view object*/
+	lv_obj_t *tabview;
+	tabview = lv_tabview_create(lv_scr_act(), NULL);
+
+	lv_obj_t *RedAutoSelect = lv_tabview_add_tab(tabview, "Red");//RedAutoSelect
+	lv_obj_t *BlueAutoSelect = lv_tabview_add_tab(tabview, "Blue");//BlueAutoSelect
+	lv_obj_t *Skillz = lv_tabview_add_tab(tabview, "Skillz");//Skills run
+	lv_obj_t *RPMSelect = lv_tabview_add_tab(tabview, "RPMSelect");//RPM selector
+
+	lv_obj_t * label = lv_label_create(RedAutoSelect, NULL);
+	lv_label_set_text(label, "Red Auto Selector\nClick the Button to switch Auto");
+
+	lv_obj_t * label2 = lv_label_create(BlueAutoSelect, NULL);
+	lv_label_set_text(label2, "Blue Auto Selector\nClick the button to switch Auto");
+
+	lv_obj_t * btn1 = lv_btn_create(RedAutoSelect, NULL);
+	lv_cont_set_fit(btn1, true, true); /*Enable resizing horizontally and vertically*/
+	lv_obj_align(btn1, label, LV_ALIGN_OUT_BOTTOM_LEFT, 0, 10);
+	lv_obj_set_free_num(btn1, 1);   /*Set a unique number for the button*/
+	lv_btn_set_action(btn1, LV_BTN_ACTION_CLICK, btn_click_action);
+
+	/*Add a label to the button*/
+	label = lv_label_create(btn1, NULL);
+	lv_label_set_text(label, "Back");
+
+	/*Button two*/
+
+	lv_obj_t * btn2 = lv_btn_create(RedAutoSelect, NULL);
+	lv_cont_set_fit(btn2, true, true); /*Enable resizing horizontally and vertically*/
+	lv_obj_align(btn2, label, LV_ALIGN_OUT_BOTTOM_RIGHT, 390, -50);
+	lv_obj_set_free_num(btn2, 2);/*Set a unique number for the button*/
+	lv_btn_set_action(btn2, LV_BTN_ACTION_CLICK, btn_click_action);
+
+	/*Add a label to the button*/
+	label = lv_label_create(btn2, NULL);
+	lv_label_set_text(label, "Front");
+
+	/*Button Three*/
+
+	lv_obj_t * btn3 = lv_btn_create(BlueAutoSelect, NULL);
+	lv_cont_set_fit(btn3, true, true); /*Enable resizing horizontally and vertically*/
+	lv_obj_align(btn3, label, LV_ALIGN_OUT_BOTTOM_LEFT, 80, -40);
+	lv_obj_set_free_num(btn3, 3);   /*Set a unique number for the button*/
+	lv_btn_set_action(btn3, LV_BTN_ACTION_CLICK, btn_click_action);
+
+	/*Add a label to the button*/
+	label = lv_label_create(btn3, NULL);
+	lv_label_set_text(label, "Back");
+
+	/*Button four*/
+	lv_obj_t * btn4 = lv_btn_create(BlueAutoSelect, NULL);
+	lv_cont_set_fit(btn4, true, true); /*Enable resizing horizontally and vertically*/
+	lv_obj_align(btn4, label, LV_ALIGN_OUT_BOTTOM_RIGHT, 390, -50);
+	lv_obj_set_free_num(btn4, 4);/*Set a unique number for the button*/
+	lv_btn_set_action(btn4, LV_BTN_ACTION_CLICK, btn_click_action);
+
+	/*Add a label to the button*/
+	label = lv_label_create(btn4, NULL);
+	lv_label_set_text(label, "Front");
+
+	/*Button five*/
+	lv_obj_t * btn5 = lv_btn_create(Skillz, NULL);
+	lv_cont_set_fit(btn5, true, true); /*Enable resizing horizontally and vertically*/
+	lv_obj_align(btn5, label, LV_ALIGN_OUT_BOTTOM_RIGHT, 390, -50);
+	lv_obj_set_free_num(btn5, 5);/*Set a unique number for the button*/
+	lv_btn_set_action(btn5, LV_BTN_ACTION_CLICK, btn_click_action);
+
+	/*Add a label to the button*/
+	label = lv_label_create(btn5, NULL);
+	lv_label_set_text(label, "SKILLZ");
+}
---
+++ Worlds/src/opcontrol.cpp
@@ -0,0 +1,66 @@
+#include "main.h"
+#include "api.h"
+#include "pros/rtos.h"
+#include "functions.h"
+
+
+void opcontrol() {
+ 	pros::Motor Intake(16);
+	pros::Motor Lift(5);
+  pros::Motor Puncher(6);
+  pros::ADIAnalogIn AnglePot (2);
+	pros::ADIAnalogIn IntakeLine_Top1 (3);
+  pros::ADIGyro gyro (4);
+
+	pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+ 	Intake.set_brake_mode(MOTOR_BRAKE_HOLD);
+	Lift.set_brake_mode(MOTOR_BRAKE_HOLD);
+
+  pros::Task puncher_task(anglerTask_fn,(void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "");
+  pros::Task drive_task(Drive, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "");
+  pros::Task puncher_task2(puncher,(void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "");
+
+
+  // Gui();
+
+  if(master.get_digital(DIGITAL_DOWN)){
+    pros::Task puncher_task(&anglerTask_fn);
+  }
+
+ 	AnglePot.calibrate();
+
+
+ 	while (true) {
+
+
+		if(master.get_digital(DIGITAL_R1)){
+			Lift.move_velocity(200);
+		}
+
+		else if(master.get_digital(DIGITAL_R2)){
+			Lift.move_velocity(-200);
+		}
+
+		else{
+			Lift.move_velocity(0);
+		}
+
+	 if(master.get_digital(DIGITAL_L1)){
+		 Intake.move_velocity(200);
+	 }
+
+	 else if(master.get_digital(DIGITAL_L2)){
+		 Intake.move_velocity(-200);
+	 }
+
+	 else{
+		 Intake.move_velocity(0);
+		}
+
+ 		int AngleRead = AnglePot.get_value_calibrated();
+ 		printf("Angle Potentiometer reading: %d\n", AnglePot.get_value_calibrated());
+    pros::delay(20);
+ 		//////////////////////////////////////
+ 	  }
+  }
