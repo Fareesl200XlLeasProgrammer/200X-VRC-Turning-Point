c87e4a8f3d0547766d3639d3735717d3496092c7
Farees|200X|LeadProgrammer <44035032+Fareesl200XlLeasProgrammer@users.noreply.github.com>
2019-03-30 23:41:28 -0400

    doesnt work

--- 200X-VRC-Turning-Point/Worlds/src/autonomous.cpp
+++ 200X-VRC-Turning-Point/Worlds/src/autonomous.cpp
@@ -17,6 +17,46 @@ using namespace pros;
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+ // void gyroTurn(double target){
+ //   pros::Motor Intake(16);
+ // 	pros::Motor Lift(5);
+ // 	pros::ADIAnalogIn IntakeLine_Top1 (3);
+ //   pros::ADIGyro gyro (4);
+ //
+ //   pros::Motor LeftF(10);
+ //   pros::Motor LeftB(1);
+ //   pros::Motor RightF(20, true);
+ //   pros::Motor RightB(11, true);
+ //
+ //   // double target = 900;
+ //   double kP = 0.3;
+ //   double kI = 0.0000003;
+ //   double kD = 1.3;
+ //   double error;
+ //   double totalError;
+ //   double lastError;
+ //   double pwr;
+ //   gyro.reset();
+ //
+ //     double current = gyro.get_value();
+ //     error = target - current;
+ //     double P = error * kP;
+ //     totalError += error;
+ //     lastError = error;
+ //
+ //     double I = kI * totalError;
+ //     double D = kD * (error - lastError);
+ //     pwr = P+I+D;
+ //
+ //     if(error == 0){
+ //       I = 0;
+ //     }
+ //
+ //       LeftF.move_velocity(pwr);
+ //       LeftB.move_velocity(pwr);
+ //       RightF.move_velocity(-pwr);
+ //       RightB.move_velocity(-pwr);
+ // }
 
 
 void autonomous() {
@@ -65,51 +105,53 @@ void autonomous() {
   //   /** Call Auto Function*/
   // }
 
-  profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{4_ft, 0_ft, 0_deg}}, "firstCap");
-  profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{4_ft, 0_ft, 0_deg}}, "firstCap2");
+  // profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{4_ft, 0_ft, 0_deg}}, "firstCap");
+  // profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{4_ft, 0_ft, 0_deg}}, "firstCap2");
+  //
+  // profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{5_ft, 0_ft, 0_deg}}, "firstCapBack");
+  // profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{1_ft, 0_ft, 0_deg}}, "firstCapadjust");
+  // profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{1.4_ft, 0_ft, 0_deg}}, "shootAdjust");
+  //
+  // profileController.setTarget("firstCap");
+  // Lift.move_absolute(-400, 200);
+  // Intake.move_velocity(-200);
+  // Puncher.move_absolute(600, 200);
+  //
+  // profileController.waitUntilSettled();
+  // profileController.setTarget("firstCapBack",true);
+  // Intake.move_velocity(0);
+  // profileController.waitUntilSettled();
+  // profileController.setTarget("firstCapadjust");
+  // profileController.waitUntilSettled();
+  // while(GyroAuto.get_value() < 700){
+  //   LeftF.move_velocity(200);
+  //   LeftB.move_velocity(200);
+  //   RightF.move_velocity(-200);
+  //   RightB.move_velocity(-200);
+  // }
+  // LeftF.move_velocity(0);
+  // LeftB.move_velocity(0);
+  // RightF.move_velocity(0);
+  // RightB.move_velocity(0);  profileController.setTarget("shootAdjust",true);
+  // profileController.waitUntilSettled();
+  //
+  // Angler.move_absolute(100, 200);
+  // while(Puncher.get_position() < 2000){
+  //   Puncher.move_velocity(200);
+  // }
+  // Puncher.move_velocity(0);
+  // Puncher.move_velocity(0);
+  // Intake.move_velocity(-200);
+  // Angler.move_absolute(160, 200);
+  // // pros::delay(500);
+  // Puncher.tare_position();
+  // while(Puncher.get_position() < 2000){
+  //   Puncher.move_velocity(200);
+  // }
+  // Puncher.move_velocity(0);
 
-  profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{5_ft, 0_ft, 0_deg}}, "firstCapBack");
-  profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{1_ft, 0_ft, 0_deg}}, "firstCapadjust");
-  profileController.generatePath({Point{0_ft, 0_ft, 0_deg}, Point{1.4_ft, 0_ft, 0_deg}}, "shootAdjust");
 
-  profileController.setTarget("firstCap");
-  Lift.move_absolute(-400, 200);
-  Intake.move_velocity(-200);
-  Puncher.move_absolute(600, 200);
-
-  profileController.waitUntilSettled();
-  profileController.setTarget("firstCapBack",true);
-  Intake.move_velocity(0);
-  profileController.waitUntilSettled();
-  profileController.setTarget("firstCapadjust");
-  profileController.waitUntilSettled();
-  while(GyroAuto.get_value() < 700){
-    LeftF.move_velocity(200);
-    LeftB.move_velocity(200);
-    RightF.move_velocity(-200);
-    RightB.move_velocity(-200);
-  }
-  LeftF.move_velocity(0);
-  LeftB.move_velocity(0);
-  RightF.move_velocity(0);
-  RightB.move_velocity(0);  profileController.setTarget("shootAdjust",true);
-  profileController.waitUntilSettled();
-
-  Angler.move_absolute(100, 200);
-  while(Puncher.get_position() < 2000){
-    Puncher.move_velocity(200);
-  }
-  Puncher.move_velocity(0);
-  Puncher.move_velocity(0);
-  Intake.move_velocity(-200);
-  Angler.move_absolute(160, 200);
-  // pros::delay(500);
-  Puncher.tare_position();
-  while(Puncher.get_position() < 2000){
-    Puncher.move_velocity(200);
-  }
-  Puncher.move_velocity(0);
-
+  // gyroTurn(-450);
 
 
 
