a9a9dbd06c918d45a3bd1469c1ac1c50ff041469
Farees <fareesahmedsiddiqui@gmail.com>
2020-03-18 22:30:33 -0400

    rEaSeArCh

---
+++ Dev/VRC5225A-2019-2020-new-robot/main/src/angler.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include "main.h"
+#include "config.hpp"
+
+#include "fBar.hpp"
+#include "drive.hpp"
+#include "controller.hpp"
+#include "logging.hpp"
+#include "tracking.hpp"
+#include <iostream>
+#include <cmath>
+#include <memory>
+#include <vector>
+using namespace std;
+using namespace pros;
+constexpr double ANGLER_TOP = 8000;
+constexpr double ANGLER_MID = 4000;
+
+enum class anglerStates {
+  Idle,
+  Mid,
+  DriveAround,
+  Push,
+  CubeOutLast,
+  BetweenTop,
+  CubeOutFirst,
+  Top
+};
+
+class Angler: public pros::Motor {
+public:
+  using Motor::Motor;
+  // int32_t move_absolute( double position, int32_t velocity );
+  double getTarget();
+private:
+  double target;
+  int32_t velocityCap;
+};
+
+extern Angler angler;
+
+void intakeOn();
+void intakeReverse();
+void startDropOff();
+void stopDropOff();
+void dropOff(void *param);
+void setAnglerState(anglerStates state);
+void anglerCal();
+void anglerHandle();
+extern anglerStates anglerState ;
+extern anglerStates anglerStateLast;
+extern int anglerStateChangeTime;
---
+++ Dev/VRC5225A-2019-2020-new-robot/main/src/config.cpp
@@ -0,0 +1,11 @@
+#include "config.hpp"
+ADIEncoder leftencoder (1, 2, true);
+ADIEncoder rightencoder (5, 6, false);
+ADIEncoder backencoder (3, 4, true);
+ADIAnalogIn topLs(7);
+ADIAnalogIn bottomLs(8);
+Vision vision(5);
+Motor intakeR(18,false), intakeL(2,false);
+Motor fBar(5, true);
+Motor front_L(1, false), front_R(10, true), back_L(11, false), back_R(20, true);
+Controller master(E_CONTROLLER_MASTER);
